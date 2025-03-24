d112568bd3f88d92d977005aba879e414c56d928
MarquinhosKadosh <proverbios16a@gmail.com>
2025-03-24 09:39:49 -0300

    inclui motivo na lista

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ async function carregarReservas() {
                 <td>${reserva.sala}</td>
                 <td>${formatarData(reserva.data)}</td>
                 <td>${reserva.periodo}</td>
+                <td>${reserva.motivo}</td>
                 <td><button class="delete-btn" onclick="excluirReserva(${reserva.id})">Excluir</button></td>
             `;
         });
@@ -73,13 +74,14 @@ async function reservarSala() {
     const sala = document.getElementById("sala").value;
     const data = document.getElementById("data").value;
     const periodo = document.getElementById("periodo").value;
+    const motivo = document.getElementById("motivo").value;
 
-    if (!usuario || !geracao || !sala || !data || !periodo) {
+    if (!usuario || !geracao || !sala || !data || !periodo || !motivo) {
         alert("Por favor, preencha todos os campos.");
         return;
     }
 
-    const reserva = { usuario, geracao, sala, data, periodo };
+    const reserva = { usuario, geracao, sala, data, periodo, motivo };
 
     try {
         const response = await fetch("https://reserva-salas-production.up.railway.app/reservas", {
